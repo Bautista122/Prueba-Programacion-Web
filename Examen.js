6f9aa08d216d94279f3c4bea75508489297fbada
Bautista <bautistamoyano1606@gmail.com>
2025-05-26 14:35:06 -0300

    prueba programacion web java script

---
+++ Examen.js
@@ -0,0 +1,17 @@
+// ------------------------- TEORICO -------------------------
+// 1) -------------------------
+
+// Var es una variable de Java script que puede ser re declarada y puede ser modificada 
+// let es una variable de Java script que puede ser re asignada pero no re declarada 
+// Const es una variable de Java script que una vez que esta declarada no se puede modificar por eso es constante
+
+// 2) -------------------------
+
+// For sirve para recorrer los elementos de una lista y hacer lo que quieras con el 
+// ForEach recorre un array y ejecuta una funcion para cada elemeto
+// Map recorre cada elemento, transforma ese elemento y lo devuelve en una nueva lista 
+// Filter filtra un elemento de una lista y lo devuelve un en una lista nueva segun la condicion que le demos 
+
+// ------------------------- PRACTICO -------------------------
+ 
+
